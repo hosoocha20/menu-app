bdba025d6fee4f8435e5ce6c45488f4b7b387e7b
Hosoo Cha <hosoo.cha@gmail.com>
2024-07-11 16:09:32 +1200

    added another font

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='w-full h-[4rem] bg-my-orange-10 sticky top-0'>
-        <div>
+        <div className='text-my-black-950 text-[1.2rem] font-[600]'>
             MenuNow
         </div>
     </div>
